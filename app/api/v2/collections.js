c3b82588a8db6a447f919040788d4bb0b38aa69d
ZHOUWEI <361772818@qq.com>
2021-02-25 16:38:56 +0800

    build collections module

--- app/api/v2/collections.js
+++ app/api/v2/collections.js
@@ -4,16 +4,32 @@
  * @Autor: zhou wei
  * @Date: 2021-02-04 15:58:05
  * @LastEditors: zhou wei
- * @LastEditTime: 2021-02-08 20:12:02
+ * @LastEditTime: 2021-02-25 16:34:34
  */
 const {
   Collections
 } = require('../../models/collections')
+const {
+  Follow
+} = require('../../models/follow')
+const {
+  Article
+} = require('../../models/article')
+const {
+  Auth
+} = require('../../../middleware/auth')
 
 const Router = require('koa-router')
+const {
+  Sequelize
+} = require('sequelize')
+const {
+  User
+} = require('../../models/user')
 const router = new Router({
   prefix: '/api/v2/collections'
 })
+const Op = Sequelize.Op
 
 router.post('/add', async (ctx) => {
   const {
@@ -25,17 +41,17 @@ router.post('/add', async (ctx) => {
     arId
   }
   await Collections.findOne({
-    where: collectionObj
-  })
-  .then(res => {
-    if(!res){
-      Collections.create(collectionObj)
-    }else {
-      res.update({
-        state: 1
-      })
-    }
-  })
+      where: collectionObj
+    })
+    .then(res => {
+      if (!res) {
+        Collections.create(collectionObj)
+      } else {
+        res.update({
+          state: 1
+        })
+      }
+    })
   ctx.body = {
     code: 200,
   }
@@ -51,31 +67,78 @@ router.post('/remove', async (ctx) => {
     arId
   }
   await Collections.findOne({
-    where: collectionObj
-  })
-  .then(res => {
-    if(res) {
-      res.update({
-        state: 0
-      })
-    }
-  })
-  .catch(error => {
-    ctx.body = {
-      code: 1005,
-      msg: error
-    }
-  })
+      where: collectionObj
+    })
+    .then(res => {
+      if (res) {
+        res.update({
+          state: 0
+        })
+      }
+    })
+    .catch(error => {
+      ctx.body = {
+        code: 1005,
+        msg: error
+      }
+    })
   ctx.body = {
     code: 200
   }
 })
 
-router.get('/', async (ctx) => {
-  const collections = await Collections.findAll({
+router.get('/', new Auth().m, async (ctx) => {
+  const {
+    type
+  } = ctx.request.query
+  const conditions = []
+  let where
+  if (type === 'themex') {
+    const collections = await Collections.findAll({
+      where: {
+        uId: ctx.auth.uid,
+        state: 1
+      },
+      raw: true
+    })
+    where = {
+      id: {
+        [Op.in]: conditions
+      }
+    }
+    collections.forEach(item => {
+      conditions.push(item.arId)
+    })
+  } else if (type === 'following') {
+    const tos = await Follow.findAll({
+      where: {
+        uId: ctx.auth.uid
+      },
+      attributes: ['to'],
+      raw: true
+    })
+    tos.forEach(item => {
+      conditions.push(item.to)
+    })
+    where = {
+      uId: {
+        [Op.in]: conditions
+      }
+    }
+  }
+  const articles = await Article.findAll({
+    where,
+    include: ['User', 'Love'],
     raw: true
   })
-  const data = collections
+  const data = []
+  articles.forEach(item => {
+    item.avatar = item['User.avatar']
+    if (item['Love.uId'] === ctx.auth.uid && item['Love.state'] === 1) {
+      item.loveState = 1
+    }
+    data.push(item)
+  })
   ctx.body = {
     code: 200,
     data
